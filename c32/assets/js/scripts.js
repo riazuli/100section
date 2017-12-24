3e152ad224a7a2d4c81ad9131826e64b14b9716f
Riazul Islam <ri64627@gmail.com>
2017-12-24 20:13:59 +0600

    c32 done

---
+++ c32/assets/js/scripts.js
@@ -0,0 +1,22 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        $(window).scroll(function(){
+                if ($(this).scrollTop() > 100) {
+                    $('.scroll-to-top').fadeIn();
+                } else {
+                    $('.scroll-to-top').fadeOut();
+                }
+            });
+            
+            //Click event to scroll to top
+            $('.scroll-to-top').click(function(){
+                $('html, body').animate({scrollTop : 0},800);
+                return false;
+            });
+    });
+
+
+}(jQuery));	

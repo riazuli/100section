a368e2b7c50861811c214ea51e09bf5bd17de573
Riazul Islam <ri64627@gmail.com>
2018-01-05 18:49:50 +0600

    c46 done

---
+++ c46/assets/js/scripts.js
@@ -0,0 +1,19 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+    	$(".hero-slider").owlCarousel({
+            items:1,
+            loop:true,
+            nav:false,
+            dots:true,
+            autoplay:true,
+        });
+    	
+    	
+    });
+
+
+}(jQuery));	

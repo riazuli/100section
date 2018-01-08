95dc13fb2cfe57b08ba028fe94b726dba2fad2bd
riazuli <ri64627@gmail.com>
2018-01-08 14:44:47 +0600

    c49 done

---
+++ c49/assets/js/scripts.js
@@ -0,0 +1,19 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+    	$(".testimonial-carousel").owlCarousel({
+            items:1,
+            loop:true,
+            nav:false,
+            dots:true,
+            autoplay:false,
+        });
+    	
+    	
+    });
+
+
+}(jQuery));	

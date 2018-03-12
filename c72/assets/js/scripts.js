e75df3530474376ae9afacb55df560f44c7068b0
Riazul Islam <ri64627@gmail.com>
2018-03-12 10:04:34 +0600

    sdjklfjdlk

---
+++ c72/assets/js/scripts.js
@@ -0,0 +1,33 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    	$('[data-toggle="tooltip"]').tooltip();
+    	$('[data-toggle="popover"]').popover();
+
+    	var footerHeight = $(".footer").outerHeight();
+    	$(".site-wrapp").css('margin-bottom', footerHeight + 'px');
+
+    	$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+    });
+
+    $(window).scroll(function() {    
+	    var scroll = $(window).scrollTop();
+
+	     //>=, not <=
+	    if (scroll >= 100) {
+	        //clearHeader, not clearheader - caps H
+	        $(".main-menu").addClass("sticky");
+	    }else{
+
+	        $(".main-menu").removeClass("sticky");
+	    }
+	});
+
+
+}(jQuery));	

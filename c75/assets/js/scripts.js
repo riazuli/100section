4e40921c93018b9021860eb1c81c677336f4b660
Riazul Islam <ri64627@gmail.com>
2018-03-15 22:42:24 +0600

    c75

---
+++ c75/assets/js/scripts.js
@@ -0,0 +1,17 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    	$('#bar1').barfiller();
+    	$('#bar2').barfiller();
+    	$('#bar3').barfiller();
+
+    });
+
+    $(window).scroll(function() {    
+	   
+	});
+
+
+}(jQuery));	
